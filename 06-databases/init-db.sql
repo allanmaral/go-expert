b84ddda83134e7d48318b5333cddb75c403d4325
Allan Amaral <allanmaralr@gmail.com>
2023-08-29 00:29:38 -0300

    add example connecting to the database and inserting a row

--- 06-databases/init-db.sql
+++ 06-databases/init-db.sql
@@ -1,1 +1,6 @@
+-- docker compose exec mysql bash
+-- mysql -uroot -p goexpert
+-- root
+--
+-- PASTE:
 CREATE TABLE products (id VARCHAR(255), name VARCHAR(80), price DECIMAL(10,2), PRIMARY KEY(id));
