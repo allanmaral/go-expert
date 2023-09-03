6d00053c6a793a1e31fccdc72366bbbe51d82fee
Allan Amaral <allanmaralr@gmail.com>
2023-09-03 01:28:27 -0300

    change preload for join

--- 06-databases/12-belongs-to-with-gorm/main.go
+++ 06-databases/12-belongs-to-with-gorm/main.go
@@ -28,11 +28,11 @@ func main() {
 	newLogger := logger.New(
 		log.New(os.Stdout, "\n", log.LstdFlags), // io writer
 		logger.Config{
-			SlowThreshold:             time.Second,   // Slow SQL threshold
-			LogLevel:                  logger.Silent, // Log level
-			IgnoreRecordNotFoundError: true,          // Ignore ErrRecordNotFound error for logger
-			ParameterizedQueries:      true,          // Don't include params in the SQL log
-			Colorful:                  false,         // Disable color
+			SlowThreshold:             time.Second, // Slow SQL threshold
+			LogLevel:                  logger.Info, // Log level
+			IgnoreRecordNotFoundError: true,        // Ignore ErrRecordNotFound error for logger
+			ParameterizedQueries:      true,        // Don't include params in the SQL log
+			Colorful:                  false,       // Disable color
 		},
 	)
 
@@ -57,7 +57,7 @@ func main() {
 	// })
 
 	var products []Product
-	db.Preload("Category").Find(&products)
+	db.Joins("Category").Find(&products)
 	for _, product := range products {
 		fmt.Println(product.Name, product.Category.Name)
 	}
