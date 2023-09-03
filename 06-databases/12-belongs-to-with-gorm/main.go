05b6d90b9ab22fdb1721396f4668a57e664604b6
Allan Amaral <allanmaralr@gmail.com>
2023-09-03 01:21:03 -0300

    add exemplate of belongs to with gorm

---
+++ 06-databases/12-belongs-to-with-gorm/main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
+
+type Category struct {
+	ID   int `gorm:"primaryKey"`
+	Name string
+}
+
+type Product struct {
+	Id         int `gorm:"primaryKey"`
+	Name       string
+	Price      float64
+	CategoryID int
+	Category   Category
+}
+
+func main() {
+	newLogger := logger.New(
+		log.New(os.Stdout, "\n", log.LstdFlags), // io writer
+		logger.Config{
+			SlowThreshold:             time.Second,   // Slow SQL threshold
+			LogLevel:                  logger.Silent, // Log level
+			IgnoreRecordNotFoundError: true,          // Ignore ErrRecordNotFound error for logger
+			ParameterizedQueries:      true,          // Don't include params in the SQL log
+			Colorful:                  false,         // Disable color
+		},
+	)
+
+	dsn := "root:root@tcp(localhost:3306)/goexpert?charset-utf8mb4&parseTime=True"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
+		Logger: newLogger,
+	})
+	if err != nil {
+		panic(err)
+	}
+	db.AutoMigrate(&Product{}, &Category{})
+
+	// // create category
+	// category := Category{Name: "Electronics"}
+	// db.Create(&category)
+
+	// // create product
+	// db.Create(&Product{
+	// 	Name:       "Notebook",
+	// 	Price:      1000.00,
+	// 	CategoryID: category.ID,
+	// })
+
+	var products []Product
+	db.Preload("Category").Find(&products)
+	for _, product := range products {
+		fmt.Println(product.Name, product.Category.Name)
+	}
+}
