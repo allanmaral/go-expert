d0563831b306ba7221004b8eb5abd614ec0f45bb
Allan Amaral <allanmaralr@gmail.com>
2023-09-03 18:55:53 -0300

    add example of locking with gorm

---
+++ 06-databases/17-locks-with-gorm/main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"log"
+	"os"
+	"time"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"gorm.io/gorm/clause"
+	"gorm.io/gorm/logger"
+)
+
+type Category struct {
+	ID       int `gorm:"primaryKey"`
+	Name     string
+	Products []Product `gorm:"many2many:products_categories;"`
+}
+
+type Product struct {
+	Id         int `gorm:"primaryKey"`
+	Name       string
+	Price      float64
+	Categories []Category `gorm:"many2many:products_categories;"`
+}
+
+func main() {
+	newLogger := logger.New(
+		log.New(os.Stdout, "\n", log.LstdFlags), // io writer
+		logger.Config{
+			SlowThreshold:             time.Second,   // Slow SQL threshold
+			LogLevel:                  logger.Silent, // Log level
+			IgnoreRecordNotFoundError: true,          // Ignore ErrRecordNotFound error for logger
+			ParameterizedQueries:      true,          // Don't include params in the SQL log
+			Colorful:                  false,         // Disable color
+		},
+	)
+
+	dsn := "root:root@tcp(localhost:3306)/goexpert?charset-utf8mb4&parseTime=True"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
+		Logger: newLogger,
+	})
+	if err != nil {
+		panic(err)
+	}
+	db.AutoMigrate(&Product{}, &Category{})
+
+	tx := db.Begin()
+
+	var c Category
+	err = tx.Debug().Clauses(clause.Locking{Strength: "UPDATE"}).First(&c, 1).Error
+	if err != nil {
+		panic(err)
+	}
+
+	c.Name = "Sample"
+	tx.Debug().Save(&c)
+	tx.Debug().Commit()
+
+}
