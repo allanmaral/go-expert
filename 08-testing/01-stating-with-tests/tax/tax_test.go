e33cfc8f8615753cd32fbe1e39c6fb807abe2cc1
Allan Amaral <allanmaralr@gmail.com>
2023-09-12 22:08:14 -0300

    add first test example

---
+++ 08-testing/01-stating-with-tests/tax/tax_test.go
@@ -0,0 +1,27 @@
+package tax
+
+import "testing"
+
+// To run tests, use the command
+// `go test .` or `go test -v .` for a verbose run
+
+func TestCalculateTaxWithPassingAssertion(t *testing.T) {
+	amount := 500.0
+	expected := 5.0
+
+	result := CalculateTax(amount)
+
+	if result != expected {
+		t.Errorf("Expected %f, got %f instead", expected, result)
+	}
+}
+func TestCalculateTaxWithFailingAssertion(t *testing.T) {
+	amount := 500.0
+	expected := 1.0
+
+	result := CalculateTax(amount)
+
+	if result != expected {
+		t.Errorf("Expected %f, got %f instead", expected, result)
+	}
+}
