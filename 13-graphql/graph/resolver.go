48e25291a583c31b27b50646140b3823e1a1a205
Allan Amaral <allanmaralr@gmail.com>
2024-03-29 19:39:25 -0300

    add simple graphql server

---
+++ 13-graphql/graph/resolver.go
@@ -0,0 +1,11 @@
+package graph
+
+import "github.com/allanmaral/go-expert/13-graphql/internal/database"
+
+// This file will not be regenerated automatically.
+//
+// It serves as dependency injection for your app, add any dependencies you require here.
+
+type Resolver struct {
+	CategoryDB *database.Category
+}
