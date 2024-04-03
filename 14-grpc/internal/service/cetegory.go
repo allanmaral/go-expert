a266b4cd7756617107f22dfd26916d6121757df1
Allan Amaral <allanmaralr@gmail.com>
2024-04-02 23:43:15 -0300

    add initial grpc protofile and model implementation

---
+++ 14-grpc/internal/service/cetegory.go
@@ -0,0 +1,37 @@
+package service
+
+import (
+	"context"
+
+	"github.com/allanmaral/go-expert/14-grpc/internal/database"
+	"github.com/allanmaral/go-expert/14-grpc/internal/pb"
+)
+
+type CategoryService struct {
+	pb.UnimplementedCategoryServiceServer
+	CategoryDB database.Category
+}
+
+var _ (pb.CategoryServiceServer) = (*CategoryService)(nil)
+
+func NewCategoryService(categoryDB database.Category) *CategoryService {
+	return &CategoryService{
+		CategoryDB: categoryDB,
+	}
+}
+
+func (cs *CategoryService) CreateCategory(ctx context.Context, in *pb.CreateCategoryRequest) (*pb.CategoryResponse, error) {
+	category, err := cs.CategoryDB.Create(in.Name, in.Description)
+	if err != nil {
+		return nil, err
+	}
+
+	return &pb.CategoryResponse{
+		Category: &pb.Category{
+			Id:          category.ID,
+			Name:        category.Name,
+			Description: category.Description,
+		},
+	}, nil
+
+}
