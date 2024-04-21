9b30888bfe57ce7cca0ed5fe55ff55f0c4cb00c4
Allan Amaral <allanmaralr@gmail.com>
2024-04-20 23:17:42 -0300

    add base project to use dependency injection

---
+++ 19-dependency-injection/product/usecase.go
@@ -0,0 +1,13 @@
+package product
+
+type ProductUseCase struct {
+	repository *ProductRepository
+}
+
+func NewProductUseCase(repository *ProductRepository) *ProductUseCase {
+	return &ProductUseCase{repository: repository}
+}
+
+func (uc *ProductUseCase) GetProduct(id int) (Product, error) {
+	return uc.repository.GetProduct(id)
+}
