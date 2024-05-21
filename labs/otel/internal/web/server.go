b0c24d979498ba304a7b460b4b808f6ce5efed0a
Allan Amaral <allanmaralr@gmail.com>
2024-05-21 18:41:23 -0300

    add open telemetry labs files

---
+++ labs/otel/internal/web/server.go
@@ -0,0 +1,96 @@
+package web
+
+import (
+	"embed"
+	"fmt"
+	"io"
+	"net/http"
+	"text/template"
+	"time"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/propagation"
+	"go.opentelemetry.io/otel/trace"
+)
+
+//go:embed template/*
+var templateContent embed.FS
+
+type Webserver struct {
+	TemplateData *TemplateData
+}
+
+func NewServer(templateData *TemplateData) *Webserver {
+	return &Webserver{
+		TemplateData: templateData,
+	}
+}
+
+func (we *Webserver) CreateServer() *chi.Mux {
+	router := chi.NewRouter()
+
+	router.Use(middleware.RequestID)
+	router.Use(middleware.RealIP)
+	router.Use(middleware.Recoverer)
+	router.Use(middleware.Logger)
+	router.Use(middleware.Timeout(60 * time.Second))
+
+	router.Handle("/metrics", promhttp.Handler())
+	router.Get("/", we.HandleRequest)
+
+	return router
+}
+
+type TemplateData struct {
+	Title              string
+	BackgroundColor    string
+	ResponseTime       time.Duration
+	ExternalCallURL    string
+	ExternalCallMethod string
+	RequestNameOTEL    string
+	Content            string
+	OTELTracer         trace.Tracer
+}
+
+func (h *Webserver) HandleRequest(w http.ResponseWriter, r *http.Request) {
+	carrier := propagation.HeaderCarrier(r.Header)
+	ctx := r.Context()
+	ctx = otel.GetTextMapPropagator().Extract(ctx, carrier)
+	ctx, span := h.TemplateData.OTELTracer.Start(ctx, h.TemplateData.RequestNameOTEL)
+	defer span.End()
+
+	time.Sleep(time.Millisecond * h.TemplateData.ResponseTime)
+
+	if h.TemplateData.ExternalCallURL != "" {
+		req, err := http.NewRequestWithContext(ctx, "GET", h.TemplateData.ExternalCallURL, nil)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		otel.GetTextMapPropagator().Inject(ctx, propagation.HeaderCarrier(req.Header))
+
+		resp, err := http.DefaultClient.Do(req)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		defer resp.Body.Close()
+
+		bodyBytes, err := io.ReadAll(resp.Body)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		h.TemplateData.Content = string(bodyBytes)
+	}
+
+	tmpl := template.Must(template.New("index.html").ParseFS(templateContent, "template/index.html"))
+	err := tmpl.Execute(w, h.TemplateData)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("failed to execute template: %v", err), http.StatusInternalServerError)
+		return
+	}
+}
